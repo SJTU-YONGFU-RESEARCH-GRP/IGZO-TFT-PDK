0079e4ea5492086ca0734304abafd994a283906e
Jiyuxin <33852556+cecijyx@users.noreply.github.com>
2022-05-16 08:52:52 +0800

    Create inn.tcl

---
+++ Scripts/inn.tcl
@@ -0,0 +1,5 @@
+set init_gnd_net VSS
+set init_lef_file {omni_directional_tech.lef omni_directional_sc_single.lef omni_directional_sc.lef}
+set init_pwr_net VDD
+set init_verilog ${verilog_file}
+init_design
